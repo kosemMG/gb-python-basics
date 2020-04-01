035742ef3dec93c6ed9cc9731757a75c750982f7
Moshe Gelberg <glbrgm@gmail.com>
2020-04-02 02:08:11 +0300

    Initial commit

--- 3.py
+++
@@ -1,5 +0,0 @@
-n = input('Please, enter a number: ')
-
-result = int(n) + int(n + n) + int(n + n + n)
-
-print(f'The result of {n} + {n + n} + {n + n + n} is equal to {result}.')
--- 6.py
+++
@@ -1,9 +0,0 @@
-distance = float(input('Enter the distance an athlete ran at the first day: '))
-interest_distance = float(input('Enter the distance you are interested in: '))
-day = 1
-
-while distance < interest_distance:
-    distance = distance * 1.1
-    day += 1
-
-print(f'An athlete will achieve his goal (not less than {interest_distance:0.1f} km) on the {day} day.')
